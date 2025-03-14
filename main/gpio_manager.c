05c6528b49b7377c43aef0fd40b45d1562d83975
Christopher Young <christopher98young@gmail.com>
2025-03-14 09:33:23 -0400

    Added gpio manager files and initial interface

---
+++ main/gpio_manager.c
@@ -0,0 +1,23 @@
+
+// Types and Definitions
+
+// Local Variables
+
+//Global Variables
+
+// Local Functions
+
+
+//Global Functions
+void initialize_gpio(void)
+{
+
+}
+int get_touch_button_state(int *on_or_off_out)
+{
+    return 0;
+}
+int set_rgb_led_value(int r, int g, int b)
+{
+    return 0;
+}
---
+++ main/include/gpio_manager.h
@@ -0,0 +1,9 @@
+#ifndef _GPIO_MANAGER_H_
+#define _GPIO_MANAGER_H_
+
+
+void initialize_gpio(void);
+int get_touch_button_state(int *on_or_off_out);
+int set_rgb_led_value(int r, int g, int b);
+
+#endif //_GPIO_MANAGER_H_
